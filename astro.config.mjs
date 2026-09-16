0f8ce2c144ff000358826c9425915b9172a4550f
juangiraldotu-design <juangiraldotu@users.noreply.github.com>
2026-09-16 12:58:51 -0500

    ci: add GitHub Actions workflow for automatic GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: process.env.GITHUB_PAGES ? 'https://juangiraldotu-design.github.io' : undefined,
+  base: process.env.GITHUB_PAGES ? '/sexto-vento' : '/',
   integrations: [tailwind()],
   image: {
     service: {
